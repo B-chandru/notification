837fdd415a98c8f5cbd6a41f6a50073ea3f159f0
Chandru <65664480+B-chandru@users.noreply.github.com>
2021-01-19 17:00:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ function send(event){
     var date=new Date();
     if(msg===""){
         alert("Enter any message!");
-    }else
-       for (let index = 0; index <= 10; index++) {
+    }else{
         Notification.requestPermission();
         new Notification(`your best frnd 😃 (${date.toLocaleDateString()})`,{
             icon:"img/icon48.png",
             body : `${msg.toUpperCase()} (${date.toLocaleTimeString()}) `,
             tag : "message"
         });     
-       }
+        document.getElementById("message").value="";
+    }
      
     }
     
